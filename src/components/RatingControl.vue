1e7d0333032c46e593d79ec1c40c68571df74ce5
Edu <edugacv@gmail.com>
2021-02-09 13:03:20 +0100

    Done exercice

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,27 @@
+<template>
+<h1>{{question}}</h1>
+    <button type="button" :class="{ active: modelValue === 'poor' }" @click="updateModelValue('poor')">Poor</button>
+    <button type="button" :class="{ active: modelValue === 'average' }" @click="updateModelValue('average')">Average</button>
+    <button type="button" :class="{ active: modelValue === 'great' }" @click="updateModelValue('great')">Great</button>
+</template>
+
+<script>
+export default {
+    props: ['modelValue', 'question'],
+    emits: ['update:modelValue'],
+    methods: {
+        updateModelValue(option){
+            this.$emit('update:modelValue', option)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.active {
+  border-color: green;
+}
+.active button {
+  color: greenyellow;
+}
+</style>
---
+++ src/components/TheForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="submitForm">
+      <rating-control v-model="rating" question="What is your Vue skill level?"></rating-control>
+        <div>
+            <button type="submit">Submit</button>
+        </div>  
+  </form>
+</template>
+
+<script>
+import RatingControl from './RatingControl.vue'
+
+export default {
+    components: {
+        RatingControl
+    },
+    data(){
+        return {
+            rating: null,
+        }
+    },
+    methods: {
+        submitForm(){
+            console.log('Raiting is: ' + this.rating)
+            this.rating = null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
